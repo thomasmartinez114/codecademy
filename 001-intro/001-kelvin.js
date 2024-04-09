5f5209eec8abc1c6e2a739f295c89831284a6f49
Thomas Martinez <tmartinez114@gmail.com>
2024-04-09 00:23:21 -0400

    push challenges completed

---
+++ 001-intro/001-kelvin.js
@@ -0,0 +1,8 @@
+// Kelvin
+const kelvin = 0;
+// Celius conversion
+const celsius = kelvin - 273;
+// Fahrenheit conversion
+// Math.floor() to round down to decimal number
+var fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
---
+++ 001-intro/002-dogYears.js
@@ -0,0 +1,2 @@
+// Dog first 2 years count as 10.5
+// Each year after is 4 dog years
