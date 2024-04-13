a54beba659860f53d6b636ef1eeef2b819e8fefd
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 00:54:22 -0400

    parameters

--- 003-functions/001.js
+++ 003-functions/001.js
@@ -17,3 +17,12 @@ function sayThanks() {
 sayThanks();
 sayThanks();
 sayThanks();
+
+// parameters and arguments
+function sayThanks(name) {
+  console.log(
+    "Thank you for your purchase " + name + "! We appreciate your business."
+  );
+}
+
+sayThanks("Cole");
