d070710d62a562fba0eedda7574dac0958e29f46
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 15:10:10 -0400

    pop()

--- 005-arrays/001-arrays.js
+++ 005-arrays/001-arrays.js
@@ -41,3 +41,15 @@ const chores = ["wash dishes", "do laundry", "take out trash"];
 
 chores.push("cut grass", "babysit");
 console.log(chores);
+
+// .pop() method -- removes last item
+const chores = [
+  "wash dishes",
+  "do laundry",
+  "take out trash",
+  "cook dinner",
+  "mop floor",
+];
+
+chores.pop();
+console.log(chores);
