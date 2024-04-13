c18932822e6b88b272b9fc1d63a7482d78e3fe41
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 13:43:54 -0400

    .length

--- 001-arrays.js
+++ 001-arrays.js
@@ -30,3 +30,8 @@ condiments = ["Mayo"];
 console.log(condiments);
 utensils[3] = "Spoon";
 console.log(utensils);
+
+// The .length property
+const objectives = ["Learn a new language", "Read 52 books", "Run a marathon"];
+
+console.log(objectives.length);
