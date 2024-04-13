b5bacba7275eadfb7fe23b5947391f0b1ace2665
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 01:44:49 -0400

    global scope

---
+++ 004-scope/001-scope.js
@@ -0,0 +1,21 @@
+const city = "New York City";
+
+function logCitySkyline() {
+  let skyscraper = "Empire State Building";
+
+  return "The stars over the " + skyscraper + " in " + city;
+}
+
+console.log(logCitySkyline());
+
+// global scope
+
+let satellite = "The Moon";
+let galaxy = "The Milky Way";
+let stars = "North Star";
+
+function callMyNightSky() {
+  return "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+}
+
+console.log(callMyNightSky());
