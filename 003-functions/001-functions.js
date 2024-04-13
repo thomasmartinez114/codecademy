49a79e50382f5b922368b93402d54934a26b8e11
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 01:19:12 -0400

    functions

--- 003-functions/001-functions.js
+++ 003-functions/001-functions.js
@@ -42,3 +42,38 @@ function monitorCount(rows, columns) {
 const numOfMonitors = monitorCount(5, 4);
 
 console.log(numOfMonitors);
+
+// helper functions
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+
+const totalCost = costOfMonitors(5, 4);
+console.log(totalCost);
+
+// Function Expressions
+const plantNeedsWater = function (day) {
+  if (day === "Wednesday") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(plantNeedsWater("Tuesday"));
+
+// Arrow Function
+const plantNeedsWater = (day) => {
+  if (day === "Wednesday") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Concise Body Arrow Functions
+const plantNeedsWater = (day) => (day === "Wednesday" ? true : false);
