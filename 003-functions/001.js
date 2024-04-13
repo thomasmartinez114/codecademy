ad0fe48538336d4e9988247fd6559887657c7877
Thomas Martinez <tmartinez114@gmail.com>
2024-04-12 23:15:02 -0400

    calling a function

--- 003-functions/001.js
+++ 003-functions/001.js
@@ -7,3 +7,13 @@ function getReminder() {
 function greetInSpanish() {
   console.log("Buenas tardes.");
 }
+
+// calling a function
+
+function sayThanks() {
+  console.log("Thank you for your purchase! We appreciate your business.");
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
