d270639f71586a2aab95b33d70c61db98e5c2987
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 22:13:29 -0400

    repeating tasks

---
+++ 006-loops/001-loops.js
@@ -0,0 +1,6 @@
+// Repeating Tasks Manually
+var vacationSpots = ["Dominican Republic", "Las Vegas", "Ibiza"];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
