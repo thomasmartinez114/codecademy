0ce480d2cac9c0173a19e2e1468480937ab51507
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 22:18:36 -0400

    for loop

--- 006-loops/001-loops.js
+++ 006-loops/001-loops.js
@@ -4,3 +4,10 @@ var vacationSpots = ["Dominican Republic", "Las Vegas", "Ibiza"];
 console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
+
+// The For Loop
+// log 5 - 10
+
+for (let i = 5; i < 11; i++) {
+  console.log(i);
+}
