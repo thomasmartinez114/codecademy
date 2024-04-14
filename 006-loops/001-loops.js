ed3a25a99da8c944951c6a203052b0e5a0c756fb
Thomas Martinez <tmartinez114@gmail.com>
2024-04-13 23:28:16 -0400

    Notorious BIG loop

--- 006-loops/001-loops.js
+++ 006-loops/001-loops.js
@@ -66,3 +66,16 @@ do {
   cupsAdded++;
   console.log(cupsAdded);
 } while (cupsAdded < cupsOfSugarNeeded);
+
+// The break keyword
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++) {
+  if (rapperArray[i] === "Notorious B.I.G.") {
+    console.log(rapperArray[i]);
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
