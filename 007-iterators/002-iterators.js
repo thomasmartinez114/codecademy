77221a3a34d9701052d51be738d04d6623b04573
Thomas Martinez <tmartinez114@gmail.com>
2024-04-14 21:52:21 -0400

    forEach()

---
+++ 007-iterators/002-iterators.js
@@ -0,0 +1,28 @@
+// Introduction to Iterators
+const artists = ["Picasso", "Kahlo", "Matisse", "Utamaro"];
+
+artists.forEach((artist) => {
+  console.log(artist + " is one of my favorite artists.");
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map((number) => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ["desk", "chair", 5, "backpack", 3.14, 100];
+
+const onlyNumbers = things.filter((thing) => {
+  return typeof thing === "number";
+});
+
+console.log(onlyNumbers);
+
+// The .forEach() Method
+const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+fruits.forEach((fruit) => console.log("I want to eat a " + fruit));
