cd80c290fa5e2000fbb955760efbaf41f83a9336
Thomas Martinez <tmartinez114@gmail.com>
2024-04-14 21:58:59 -0400

    .map()

--- 007-iterators/002-iterators.js
+++ 007-iterators/002-iterators.js
@@ -26,3 +26,36 @@ const fruits = ["mango", "papaya", "pineapple", "apple"];
 
 // Iterate over fruits below
 fruits.forEach((fruit) => console.log("I want to eat a " + fruit));
+
+// The .map() Method
+// .map() works in a similar manner to .forEach()— the major difference is that .map() returns a new array.
+
+const animals = [
+  "Hen",
+  "elephant",
+  "llama",
+  "leopard",
+  "ostrich",
+  "Whale",
+  "octopus",
+  "rabbit",
+  "lion",
+  "dog",
+];
+
+// Create the secretMessage array below
+
+const secretMessage = animals.map((animal) => {
+  return animal[0];
+});
+
+console.log(secretMessage.join(""));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map((number) => {
+  return number / 100;
+});
+
+console.log(smallNumbers);
