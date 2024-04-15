c6557b4ad7af634cd844c0c65026f7c4430c347c
Thomas Martinez <tmartinez114@gmail.com>
2024-04-14 22:04:15 -0400

    filter()

--- 007-iterators/002-iterators.js
+++ 007-iterators/002-iterators.js
@@ -59,3 +59,28 @@ const smallNumbers = bigNumbers.map((number) => {
 });
 
 console.log(smallNumbers);
+
+// The .filter() Method
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers
+const smallNumbers = randomNumbers.filter((number) => {
+  return number < 250;
+});
+
+console.log(smallNumbers);
+
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene",
+];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter((word) => {
+  return word.length > 7;
+});
+
+console.log(longFavoriteWords);
