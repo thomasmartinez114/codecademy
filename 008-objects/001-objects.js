a513bf97b77e0ecbfe2252cc09371702b9fb0905
Thomas Martinez <tmartinez114@gmail.com>
2024-04-17 02:00:34 -0400

    methods

--- 008-objects/001-objects.js
+++ 008-objects/001-objects.js
@@ -45,3 +45,23 @@ spaceship.numEngines = 5;
 
 // delete 'Secret Mission'
 delete spaceship["Secret Mission"];
+
+///////////////////////////////////
+// Methods
+///////////////////////////////////
+
+let retreatMessage =
+  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
+
+// Write your code below
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  },
+};
+
+alienShip.retreat();
+alienShip.takeOff();
