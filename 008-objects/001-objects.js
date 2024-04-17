e4de6907c52bd65f738f0f2a9d36a9a87691cddb
Thomas Martinez <tmartinez114@gmail.com>
2024-04-17 01:56:11 -0400

    Property Assignment

--- 008-objects/001-objects.js
+++ 008-objects/001-objects.js
@@ -7,3 +7,41 @@ let fasterShip = {
   "Fuel Type": "Turbo Fuel",
   color: "silver",
 };
+
+///////////////////////////////////
+// Bracket Notation
+///////////////////////////////////
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  "Active Mission": true,
+  homePlanet: "Earth",
+  numCrew: 5,
+};
+
+let propName = "Active Mission";
+
+// Write your code below
+let isActive = spaceship["Active Mission"];
+
+console.log(isActive);
+
+///////////////////////////////////
+// Property Assignment
+///////////////////////////////////
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+  color: "silver",
+  "Secret Mission": "Discover life outside of Earth.",
+};
+
+// Write your code below
+spaceship.color = "glorious gold";
+
+// add new property to object
+spaceship.numEngines = 5;
+
+// delete 'Secret Mission'
+delete spaceship["Secret Mission"];
