deb130af066cd39978dfeb148225810ae3cb9765
Thomas Martinez <tmartinez114@gmail.com>
2024-04-17 01:38:19 -0400

    choose right iterator

--- 007-iterators/002-iterators.js
+++ 007-iterators/002-iterators.js
@@ -153,7 +153,10 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 
 console.log(newSum);
 
+///////////////////////////////////
 // Iterator Documentation
+///////////////////////////////////
+
 const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
 
 // Something is missing in the method call below
@@ -176,3 +179,40 @@ console.log(
     return word.length > 5;
   })
 );
+
+///////////////////////////////////
+// Choose the right iterator
+///////////////////////////////////
+
+const cities = [
+  "Orlando",
+  "Dubai",
+  "Edinburgh",
+  "Chennai",
+  "Accra",
+  "Denver",
+  "Eskisehir",
+  "Medellin",
+  "Yokohama",
+];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach((city) => console.log("Have you visited " + city + "?"));
+
+// Choose a method that will return a new array
+const longCities = cities.filter((city) => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0];
+}, "C");
+
+console.log(word);
+
+// Choose a method that will return a new array
+const smallerNums = nums.map((num) => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every((num) => num < 0);
