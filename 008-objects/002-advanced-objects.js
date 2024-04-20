dc23e6d8efda8514b928a230a084c156a6063afe
Thomas Martinez <tmartinez114@gmail.com>
2024-04-19 23:10:06 -0400

    privacy

--- 008-objects/002-advanced-objects.js
+++ 008-objects/002-advanced-objects.js
@@ -23,8 +23,28 @@ console.log(robot.provideInfo());
 // Replace ‘MODEL’ and ‘ENERGYLEVEL’ with the calling object’s model and energyLevel property. Remember, to get the access to the calling object’s properties inside a method, you have to use the this keyword!
 
 ///////////////////////////////////
-//
+// Privacy
 ///////////////////////////////////
+
+const robot = {
+  _energyLevel: "high",
+  recharge() {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`);
+  },
+};
+
+robot.recharge();
+
+// 1. Below the robot object, reassign the _energyLevel property to 'high'.
+
+// 2. Now take a look at the new recharge method in robot. .recharge() will add 30 to _energyLevel.
+
+// What will happen now that _energyLevel isn’t a number?
+
+// Call .recharge() on robot to find out.
+
+// Notice that a funky string is printed to the console! This is known as a side-effect of type-coercion. No need to worry about what this means for now, but it’s important to understand that you can cause unwanted side-effects when mutating objects and their properties.
 
 ///////////////////////////////////
 //
