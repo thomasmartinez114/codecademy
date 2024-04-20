df42f51dc4a2fa1075e015a8da7f489e59681d69
Thomas Martinez <tmartinez114@gmail.com>
2024-04-19 23:37:06 -0400

    getters

--- 008-objects/002-advanced-objects.js
+++ 008-objects/002-advanced-objects.js
@@ -47,8 +47,36 @@ robot.recharge();
 // Notice that a funky string is printed to the console! This is known as a side-effect of type-coercion. No need to worry about what this means for now, but it’s important to understand that you can cause unwanted side-effects when mutating objects and their properties.
 
 ///////////////////////////////////
-//
+// Getters
 ///////////////////////////////////
+
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === "number") {
+      return "My current energy level is " + this._energyLevel;
+    } else {
+      return "System malfunction: cannot retrieve energy level";
+    }
+  },
+};
+
+console.log(robot.energyLevel);
+
+// 1. In robot, create a getter method named energyLevel using the get keyword. Leave the function body blank for now.
+
+// 2. Inside the getter method, add an if statement to check if this._energyLevel is a number using the typeof operator. If that condition is truthy, return 'My current energy level is ENERGYLEVEL'. Replace ENERGYLEVEL with the value of this._energyLevel.
+
+// Make sure you return the string instead of logging it to the console.
+
+// 3. If this._energyLevel isn’t a number it could be that the _energyLevel property was altered. Let’s add a default return statement for when such a scenario arises.
+
+// Add an else statement that returns 'System malfunction: cannot retrieve energy level'.
+
+// 4. Log the result of calling the getter method energyLevel on robot to the console.
+
+// Notice that the method will return a formatted response rather than just accessing a property!
 
 ///////////////////////////////////
 //
