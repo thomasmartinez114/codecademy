a09b475f5e97e20f8276754cb2289d38dec363ac
Thomas Martinez <tmartinez114@gmail.com>
2024-04-21 12:28:21 -0400

    factory functions

--- 008-objects/002-advanced-objects.js
+++ 008-objects/002-advanced-objects.js
@@ -119,5 +119,38 @@ console.log(robot.numOfSensors);
 // 5. To check that the setter method worked, console.log() robot.numOfSensors.
 
 ///////////////////////////////////
+// Factory Functions
+///////////////////////////////////
+
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+
+const tinCan = robotFactory("P-500", true);
+tinCan.beep();
+
+// 1. Instead of making individual robots like we’ve been doing, let’s make a factory function to make robots as we please!
+
+// Create a factory function named robotFactory that has two parameters model and mobile. Make the function return an object. In the object, add a key of model with the value of the model parameter. Add another property that has a key of mobile with a value of the mobile parameter.
+
+// Then add a method named beep without a parameter that will log 'Beep Boop' to the console.
+
+// 2. Use your factory function by declaring a const variable named tinCan. Assign to tinCan the value of calling robotFactory with the first argument of 'P-500' and the second argument of true.
+
+// 3. Let’s now check what tinCan can do! Call .beep() on tinCan.
+
+// You should see 'Beep Boop' printed to the console which means the factory function produced a robot object! Play around with tinCan to check the other properties!
+
+///////////////////////////////////
+//
+///////////////////////////////////
+
+///////////////////////////////////
 //
 ///////////////////////////////////
