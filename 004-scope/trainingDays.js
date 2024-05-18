a8235caa80bacd476bcf4a9daed7fc1259a8949e
Thomas Martinez <tmartinez114@gmail.com>
2024-05-17 22:27:27 -0400

    fixed the programs scoping issues

---
+++ 004-scope/trainingDays.js
@@ -0,0 +1,52 @@
+// https://www.codecademy.com/courses/introduction-to-javascript/projects/training-days
+
+const getRandEvent = () => {
+  // The scope of `random` is too loose
+  const random = Math.floor(Math.random() * 3)
+
+  if (random === 0) {
+    return "Marathon"
+  } else if (random === 1) {
+    return "Triathlon"
+  } else if (random === 2) {
+    return "Pentathlon"
+  }
+}
+
+// The scope of `days` is too tight
+const getTrainingDays = (event) => {
+  let days = 0
+
+  if (event === "Marathon") {
+    days = 50
+  } else if (event === "Triathlon") {
+    days = 100
+  } else if (event === "Pentathlon") {
+    days = 200
+  }
+
+  return days
+}
+
+// The scope of `name` is too tight
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`)
+}
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`)
+}
+
+const event = getRandEvent()
+const days = getTrainingDays(event)
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime
+let name = "Thomas"
+
+const event2 = getRandEvent()
+const days2 = getTrainingDays(event2)
+const name2 = "Warren"
+
+logEvent(name2, event2)
+
+logEvent(name, event)
+logTime(name, days)
