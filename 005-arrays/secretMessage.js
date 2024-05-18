080cc575d176f6c01c9f812add1058b8cb42a5a9
Thomas Martinez <tmartinez114@gmail.com>
2024-05-17 23:33:35 -0400

    completed secretMessage challenge

--- 005-arrays/secretMessage.js
+++ 005-arrays/secretMessage.js
@@ -26,3 +26,25 @@ let secretMessage = [
   "Learn",
   "JavaScript",
 ]
+
+// remove last string of array
+secretMessage.pop()
+
+// add 'to' and 'Program' to end of arr separately
+secretMessage.push("to")
+secretMessage.push("Program")
+
+// Change the word easily to the word right by accessing the index and replacing it.
+secretMessage[7] = "right"
+
+// Use an array method to remove the first string of the array.
+secretMessage.shift()
+
+// Use an array method to add the string Programming to the beginning of the array.
+secretMessage.unshift("Programming")
+
+// Use an array method to remove the strings get, right, the, first, time,, and replace them with the single string know,.
+secretMessage.splice(6, 5, "know,")
+
+console.log(secretMessage)
+console.log(secretMessage.join(" "))
