dba47e03554142efb1b73f1cc6dd3bc597715bac
Thomas Martinez <tmartinez114@gmail.com>
2024-05-23 23:06:43 -0400

    completed mealMaker challenge

--- 008-objects/mealMaker.js
+++ 008-objects/mealMaker.js
@@ -8,21 +8,39 @@ let menu = {
   _meal: "",
   _price: 0,
 
+  // Getter to retrieve
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return `Today's Special is ${this._meal} for $${this._price}`
+    } else {
+      return `Meal or price was not set correctly!`
+    }
+  },
+
+  // set meal
   set meal(mealToCheck) {
     if (typeof mealToCheck === "string") {
       this._meal = mealToCheck
       return this._meal
+    } else {
+      console.log("Meal must be a String")
     }
   },
 
+  // set price
   set price(priceToCheck) {
     if (typeof priceToCheck === "number") {
       this._price = priceToCheck
       return this._price
+    } else {
+      console.log("Must be a number")
     }
   },
 }
 
-menu.meal = "Thomas"
+menu.meal = "244"
 menu.price = 10
 console.log(menu)
+
+// console.log getter method
+console.log(menu.todaysSpecial)
