f66d0d6fc3e3a1efd23e08a9b0e71c2d18faa669
Thomas Martinez <tmartinez114@gmail.com>
2024-05-25 09:30:49 -0400

    completed methods

---
+++ 002-learn-javascript-intermediate/001-classes/003-methods.js
@@ -0,0 +1,54 @@
+// In the Surgeon constructor, prepend the name and department properties with an underscore (_).
+
+// Checkpoint 2 Step instruction is unavailable until previous steps are completed
+// 2.
+// Inside of the constructor(), add a property named _remainingVacationDays and set it equal to 20.
+
+// Checkpoint 3 Step instruction is unavailable until previous steps are completed
+// 3.
+// Under the constructor(), create a getter called name that returns the value saved to _name.
+
+// Checkpoint 4 Step instruction is unavailable until previous steps are completed
+// 4.
+// Under the name getter, create a getter called department that returns the value saved to _department.
+
+// Checkpoint 5 Step instruction is unavailable until previous steps are completed
+// 5.
+// Under the department getter, create a getter called remainingVacationDays that returns the value saved to _remainingVacationDays.
+
+// Checkpoint 6 Step instruction is unavailable until previous steps are completed
+// 6.
+// Under the remainingVacationDays getter, create a method called takeVacationDays that accepts one argument named daysOff.
+
+// Inside of the method, subtract daysOff from the number saved to _remainingVacationDays. Set _remainingVacationDays to the result.
+
+class Surgeon {
+  constructor(name, department) {
+    this._name = name
+    this._department = department
+    this._remainingVacationDays = 20
+  }
+
+  // getter that returns name
+  get name() {
+    return this._name
+  }
+
+  // getter return department
+  get department() {
+    return this._department
+  }
+
+  // getter _remainingVacationDays
+  get remainingVacationDays() {
+    return this._remainingVacationDays
+  }
+
+  //
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff
+  }
+}
+
+const surgeonRomero = new Surgeon("Francisco Romero", "Cardiovascular")
+const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics")
